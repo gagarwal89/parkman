3c96eeea6b342402ffb7552be4da6c4436414cb6
Gaurav-Personal <gaurav@Gaurav-Mac.local>
2016-04-30 10:18:40 -0700

    Fixing twitter gateway with cursor

--- gateways/twitter.py
+++ gateways/twitter.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
 from parkman.config import config
 import tweepy
-
+from tweepy import (
+    TweepError
+)
 
 class TwitterGateway:
     def __init__(self):
@@ -13,23 +15,29 @@ class TwitterGateway:
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
 
-        self.api = tweepy.API(auth)
+        self.api = tweepy.API(auth, wait_on_rate_limit=True)
 
     def get_my_tweets(self):
         public_tweets = self.api.home_timeline()
         return public_tweets
 
     def get_original_tweet(self, screen_name, number_of_tweets):
-        statuses = self.api.user_timeline(screen_name=screen_name, count=number_of_tweets)
-        original_ids = []
-        for status in statuses:
+        original_tweets = []
+        for status in tweepy.Cursor(self.api.user_timeline, screen_name=screen_name).items(number_of_tweets):
+            
+            # process status here
             if status.in_reply_to_status_id:
-                original_ids.append(status.in_reply_to_status_id)
+                original_tweet = self.process_status(status)
 
-        original_statuses = self.api.statuses_lookup(
-            original_ids, 
-            include_entities=False,
-            trim_user=True)
+                if original_tweet:
+                    print original_tweet
 
-        for question in original_statuses:
-            print question.text
+    def process_status(self, status):
+        if not status.in_reply_to_status_id:
+            return status.text
+        else:
+            try:
+                status = self.api.get_status(status.in_reply_to_status_id)
+                return self.process_status(status)
+            except TweepError as e:
+                return None
